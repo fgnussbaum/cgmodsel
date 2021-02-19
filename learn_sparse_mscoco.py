15ae21b53a2eb872a1e09b0a577f7da78415a67d
Frank Nussbaum <frank.nussbau,@uni-jena.de>
2021-02-19 14:33:58 +0100

    amy

--- learn_sparse_mscoco.py
+++ learn_sparse_mscoco.py
@@ -81,7 +81,7 @@ def learn_sparse_model(logger, opts,
 #        if alpha == 0:
 #            alpha += 1e-3
 #        gamma = alpha / (1 - alpha)
-        gamma = 0.01
+        gamma = 1
         t1 = time.time()
         solver.set_regularization_params(gamma)
         out = solver.solve(verb=solver_verb, 
