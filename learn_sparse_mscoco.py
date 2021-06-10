fc0865fa8c30c36fe1883885578109c9becc6847
Frank Nussbaum <frank.nussbau,@uni-jena.de>
2021-06-10 21:19:19 +0200

    amy

--- learn_sparse_mscoco.py
+++ learn_sparse_mscoco.py
@@ -283,7 +283,7 @@ if __name__ == '__main__':
     srange = end, steps, frac
     opts = {'maxiter':1200}
     model = learn_sparse_model(logger, opts, solver_verb=1,
-                               gamma=2.5, wc=.5,
+                               gamma=2.5, wc=None,
                                dataname = 'mscoco.5000')
     
 
--- mscoco_model_eval.py
+++ mscoco_model_eval.py
@@ -32,7 +32,8 @@ def model_structure():
     infile = "mscoco.5000_ga10.00_wc0.10.pw" # (0,91)
     infile = "mscoco.5000_ga10.00_wc0.20.pw" # (0,537)
     infile = "mscoco.5000_ga5.00_wc0.25.pw" # (0, 706, 235)
-    infile = "mscoco.5000_ga5.00_wc0.50.pw"
+    infile = "mscoco.5000_ga5.00_wc0.50.pw" # (0,467)
+    infile = "mscoco.5000_ga2.50_wc0.50.pw" # (0,636)
     
 #    infile = "mscoco.train2_ga20.00.pw" # wrong data, (1053,0)@1e-2
 #    infile = "mscocomodels/cifar10.50000_ga20.00.pw"
