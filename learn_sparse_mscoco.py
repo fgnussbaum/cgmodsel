32007ab3a168117595a7bc1e8da6c98d68250912
Frank Nussbaum <frank.nussbau,@uni-jena.de>
2021-06-22 08:46:47 +0200

    amy

--- learn_sparse_mscoco.py
+++ learn_sparse_mscoco.py
@@ -283,7 +283,7 @@ if __name__ == '__main__':
     srange = end, steps, frac
     opts = {'maxiter':1200}
     model = learn_sparse_model(logger, opts, solver_verb=1,
-                               gamma=50, wc=.15,
+                               gamma=55, wc=.08,
                                dataname = 'mscoco.train2')
     
 
