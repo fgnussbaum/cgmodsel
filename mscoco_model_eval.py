f4d1c6aba526db30c4acd4aef5739c70e98c7d4b
Frank Nussbaum <frank.nussbau,@uni-jena.de>
2021-06-29 22:31:02 +0200

    amy

--- mscoco_model_eval.py
+++ mscoco_model_eval.py
@@ -39,6 +39,7 @@ def model_structure():
 #    infile = "mscoco.5000_ga3.50_wc0.75.pw" # (142, 408)
     infile = "mscoco.5000_ga5.00_wc1.00.pw"
     infile = "mscoco.5000_ga2.00_wc1.00.pw"
+    infile = "mscoco.5000_ga0.50_wc1.00.pw"
     
 #    infile = "mscoco.train2_ga20.00_wc0.75.pw" # (1039,0)
 #    infile = "mscoco.train2_ga35.00_wc0.25.pw" # (1092, 2)
