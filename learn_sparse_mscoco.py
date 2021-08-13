1db31a3eb47a6e12c815e05776c4885cc8c15a82
Frank Nussbaum <frank.nussbau,@uni-jena.de>
2021-08-13 15:14:33 +0200

    rub

--- learn_sparse_mscoco.py
+++ learn_sparse_mscoco.py
@@ -310,7 +310,7 @@ if __name__ == '__main__':
     hostname = os.gethostname()
     if hostname == 'amy':
         gamma = 25; wc =.1;  dataname = 'mscoco.train2'
-    elif hostname == 'rubrecht:
+    elif hostname == 'rubrecht':
         gamma = 1; wc =1; dataname = 'mscoco.train2_2'
     elif hostname == 'raj':
         gamma = .2; wc =1; dataname = 'mscoco.5000'
