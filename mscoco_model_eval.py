881fdd80d8373d1f3e0566d203ca0461385c81ca
Frank Nussbaum <frank.nussbau,@uni-jena.de>
2021-08-19 22:03:19 +0200

    rub

--- mscoco_model_eval.py
+++ mscoco_model_eval.py
@@ -70,13 +70,14 @@ def model_structure(checksample=True, remedge=None):
     infile = "mscoco.train2_s_ga1.20_wc0.01_u1_crf1.pw" # (17)
 #    infile = "mscoco.train2_s_ga1.10_wc0.01_u1_crf1.pw" # 25
 #    infile = "mscoco.train2_s_ga1.05_wc0.01_u1_crf1.pw" # 25
+    infile = "mscoco.train2_s_ga0.10_wc0.01_u1_crf1_off1.pw" # 1050
     
-    infile = "mscoco.train2_s_ga1.25_wc0.01_u1_crf1_off0.pw"
+#    infile = "mscoco.train2_s_ga1.25_wc0.01_u1_crf1_off0.pw"
 #    infile = "mscoco.train2_s_ga1.40_wc0.01_u1_crf1_off0.pw"
 #    infile = "mscoco.train2_s_ga1.50_wc0.01_u1_crf1_off0.pw"
-    infile = "mscoco.train2_s_ga1.20_wc0.01_u1_crf1_off0.pw" # 36
-    infile = "mscoco.train2_s_ga1.10_wc0.01_u1_crf1_off0.pw" # 40
-    infile = "mscoco.train2_s_ga1.00_wc0.01_u1_crf1_off0.pw" # 48
+#    infile = "mscoco.train2_s_ga1.20_wc0.01_u1_crf1_off0.pw" # 36
+#    infile = "mscoco.train2_s_ga1.10_wc0.01_u1_crf1_off0.pw" # 40
+#    infile = "mscoco.train2_s_ga1.00_wc0.01_u1_crf1_off0.pw" # 48
     
 #    infile = "mscoco.train2_ga50.00_wc0.10.pw" # new 400 iter (0, 571)
 #    infile = "mscoco.train2_ga40.00_wc0.10.pw" # new 216 iter (0, 638)
